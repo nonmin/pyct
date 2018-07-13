f4cad977c5a937b6203a9baf7230c0f07e763859
wohenqiong <lix3@ornl.gov>
2018-07-12 23:24:42 -0400

    Update pyct.py

--- pyct.py
+++ pyct.py
@@ -1,6 +1,6 @@
 ######################
 # Python implementation of algorithm in
-# Buades el al: Fast Cartoon + Texture Image Filters,IEEE Image Processing
+# Buades el al: Fast Cartoon + Texture Image Filters,IEEE Image Processing (2010)
 #######################
 import numpy as np
 from scipy.misc import imread,imsave
